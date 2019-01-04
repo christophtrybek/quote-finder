0e927b6675a320c0be350410e1cd0838b04a5bfa
christophtrybek <christoph.trybek@gmail.com>
2019-01-04 11:43:54 +0100

    Added helper function to retrieve correct movie quotes

--- js/quote-finder.js
+++ js/quote-finder.js
@@ -28,6 +28,7 @@ function search(searchTerm){
 
 /**
  * Get the cast and the quotes from 
+ * @param {int} movieID
  * @param {string} movieName 
  */
 function searchDetails(movieID, movieName){
@@ -44,7 +45,7 @@ function searchDetails(movieID, movieName){
     
     axios({
         method: 'get',
-        url: 'http://movie-quotes-app.herokuapp.com/api/v1/quotes?movie=' + searchTerm,
+        url: 'http://movie-quotes-app.herokuapp.com/api/v1/quotes?movie=' + sluggify(movieName),
         headers: {
             Authorization: 'Token token=' + movieQuotesApiKey
         }
@@ -55,4 +56,12 @@ function searchDetails(movieID, movieName){
         .catch((err) => {
             console.log(err, 'Error');
         })    
+}
+
+/**
+ * Transforms a string into a slug
+ * @param {string} string 
+ */
+function sluggify(string){
+    return string.replace(' ', '-').toLowerCase();
 }
